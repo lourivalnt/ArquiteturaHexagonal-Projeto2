0637273aa085e18cf245719234c19729dade7704
Lourival Linard <l.linard@gmail.com>
2021-01-29 08:59:08 -0300

    Projeto 2 - Aula 6

---
+++ src/main/java/tela/AdaptadorJavaFx.java
@@ -0,0 +1,29 @@
+package tela;
+
+import dsv.Build2;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+// Responsável por fazer o ponto de inicio de execução
+public class AdaptadorJavaFx extends Application {
+
+    private ApplicationContext spring;
+
+    @Override
+    public void init() {
+        System.out.println("iniciando spring..");
+        spring = new AnnotationConfigApplicationContext(Build2.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        var form = spring.getBean(conta.tela.TransferenciaFrm.class);
+        form.mostrar(stage);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
