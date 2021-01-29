c1ed260fce6d598443c468049801a298ef4bf29b
Lourival Linard <l.linard@gmail.com>
2021-01-28 23:25:37 -0300

    Projeto 2 - Aula 4

---
+++ src/main/java/dsv/Build2.java
@@ -0,0 +1,18 @@
+package dsv;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// Responsável por configurar os serviços do spring
+@Configuration
+@ComponentScan({
+        // adptadores front-end javafx
+        "conta.dsv",
+        "conta.tela",
+        // objetos do sistema
+        "conta.sistema",
+        // adptadores falsos
+        "conta.adaptador"})
+public class Build2 {
+    // Build 2 - Adaptador JavaFX -> Sistema <- Adaptadores Mocks
+}
